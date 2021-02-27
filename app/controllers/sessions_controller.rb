94a42df17f8936828bf3b25d686e30ae4db3a6bc
SysyHu <h.sysy.hu@gmail.com>
2021-02-27 12:04:32 +0800

    登录页面

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+  	@user = User.find_by(username: params[:username])
+  	if @user 
+  		session[:user_id] = @user.id
+  		flash[:notice] = "登录成功"
+  		redirect_to root_path
+  	else
+  		flash[:notice] = "登录失败"
+  		render action: :new
+  	end
+  end
 end
