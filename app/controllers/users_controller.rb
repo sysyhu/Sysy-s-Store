bff1064eed8146e5c81a9e830b12dff5d8d0d036
SysyHu <h.sysy.hu@gmail.com>
2021-02-27 10:58:47 +0800

    用户注册功能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,22 @@ class UsersController < ApplicationController
   end
 
   def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_attr)
+  	if @user.save
+  		flash[:notice] = "注册成功，请登录"
+  		redirect_to new_session_path
+  	else
+  		flash[:notice] = "注册失败，请再试一次"
+  		render action: :new
+  	end
+  end
+
+  private
+  def user_attr
+  	params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ApplicationRecord
 
 	#validations
 	validates :username, presence: {message: "用户名不能为空"}
+	validates :username, uniqueness: {message: "该用户名已被注册，请重试"}
 
-	validates :email, presence: {message: "邮箱不能为空",  with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/}
+	validates :email, presence: {message: "邮箱不能为空"}
+	validates :email, format: {with: /\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/, message: "请输入合法邮箱格式"}
 	validates :email, uniqueness: {message: "该邮箱已被注册，请重试"}
 
 	validates :password, presence: {message: "密码不能为空"}
+	validates :password, length: {minimum: 6, message: "密码至少6位"}
 	validates :password, confirmation: {message: "密码不一致"}
 	validates :password_confirmation, presence: {message: "确认密码不能为空"}
 
