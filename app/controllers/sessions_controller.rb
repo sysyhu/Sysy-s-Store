4fa3c24940493b2c7223384ac151617134baa35a
SysyHu <h.sysy.hu@gmail.com>
2021-02-27 16:28:09 +0800

    登录、退出、当前用户、已登录？功能

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,15 @@
 class SessionsController < ApplicationController
+
+	helper_method :current_user
+	helper_method :logged_in?
+
   def new
   end
 
   def create
   	@user = User.authenticate(params[:username], params[:password])
   	if @user 
-  		session[:user_id] = @user.id
+  		login @user
   		flash[:notice] = "登录成功"
   		redirect_to root_path
   	else
@@ -15,6 +19,34 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-
+  	logout
+  	flash[:notice] = "退出成功"
+  	redirect_to root_path
   end
+
+  private
+  #登录
+  def login user 
+  	session[:user_id] = user.id
+  end
+
+  #退出
+  def logout
+  	session[:user_id] = nil
+  end
+
+  #是否登录？
+  def logged_in?
+  	!session[:user_id].nil?
+  end
+
+  #当前用户
+  def current_user
+  	if logged_in?
+  		@current_user ||= User.find(session[:user_id])
+  	else
+  		nil
+  	end
+  end
+
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+	def logged_in?
+  	!session[:user_id].nil?
+  end
+
+  def current_user
+  	if logged_in?
+  		@current_user ||= User.find(session[:user_id])
+  	else
+  		nil
+  	end
+  end
 end
