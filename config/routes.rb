fe4ce2540bc4b1bf6445d0984c87c777ced502cd
SysyHu <h.sysy.hu@gmail.com>
2021-02-28 12:48:43 +0800

    admin基本配置:命名空间(controller, view, assets)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,13 @@ Rails.application.routes.draw do
   
   resources :sessions
 
+  namespace :admin do
+  	root 'sessions#new'
+  	resources :sessions
+  	resources :categories
+  end
+
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
