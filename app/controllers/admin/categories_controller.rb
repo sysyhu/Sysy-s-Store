56f80019693b6a6664ad8b5a832b5c06ff44d586
SysyHu <h.sysy.hu@gmail.com>
2021-03-01 11:07:54 +0800

    categories controller #edit, #update, #destroy

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,12 +1,16 @@
 class Admin::CategoriesController < Admin::BaseController
+
+	before_action :find_root_categories, only: [:new, :create, :edit, :update]
+	before_action :find_category, only: [:edit, :update, :destroy]
+
   def new
   	@category = Category.new
-  	@root_categories = Category.roots.order(id: "desc")
+  	find_root_categories
   end
 
   def create
   	@category = Category.new(category_attr)
-  	@root_categories = Category.roots.order(id: "desc")
+  	find_root_categories
   	if @category.save
   		flash[:notice] = "创建成功"
   		redirect_to admin_categories_path
@@ -20,14 +24,55 @@ class Admin::CategoriesController < Admin::BaseController
   	@categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
   end
 
+  def edit
+  	find_category
+  	find_root_categories
+  	render action: :new
+  end
+
   def update
+  	find_category
+  	find_root_categories
+  	@category.update_attributes category_attr
+  	if @category.save
+  		flash[:notice] = "修改成功"
+  		redirect_to admin_categories_path
+  	else
+  		flash[:notice] = "修改失败，请重试"
+  		render action: :new
+  	end
   end
 
   def destroy
+  	find_category
+  	if @category.destroy
+  		flash[:notice] = "删除成功"
+  		redirect_to admin_categories_path
+  	else
+  		flash[:notice] = "删除失败，请重试"
+  		redirect_to :back
+  	end
   end
 
   private
   def category_attr
   	params.require(:category).permit!
   end
+
+  def find_root_categories
+  	@root_categories = Category.roots.order(id: "desc")
+  end
+
+  def find_category
+  	@category = Category.find(params[:id])
+  end
 end
+
+
+
+
+
+
+
+
+
--- app/models/category.rb
+++ app/models/category.rb
@@ -7,6 +7,13 @@ class Category < ApplicationRecord
 	#validations
 	validates :title, presence: { message: "名字不能为空"}
 
+	before_validation :correct_ancestry
+
+	private
+	def correct_ancestry
+		self.ancestry = nil if self.ancestry.blank?
+	end
+
 
 
 end
